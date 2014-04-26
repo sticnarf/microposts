be1f2aeb81c6e0ce03f14659563f6ee5c8b5e946
Yilin Chen <wormful@gmail.com>
2014-04-26 14:48:47 +0800

    Fix email regexp defect

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,14 @@
 class User < ActiveRecord::Base
   NAME_MAX_LENGTH = 50
+  EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :name, :email, presence: true
   validates :name, :length => { maximum: NAME_MAX_LENGTH }
-  validates :email, :format => { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, :format => { with: EMAIL_REGEXP }
   validates :email, :uniqueness => { case_sensitive: false }
   validates :password, :length => { minimum: 6 }
 
   has_secure_password
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 end
