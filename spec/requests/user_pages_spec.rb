b800cc4291a1e8adf63903437cee3a50beb0624b
Yilin Chen <wormful@gmail.com>
2014-05-02 21:12:30 +0800

    Add tests for alerts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -17,6 +17,13 @@ describe "User Pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -28,6 +35,13 @@ describe "User Pages" do
       end
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: "Welcome") }
       end
     end
 
